b764df56679830a7bf1961955b9bc29e8a7eba63
Андрей Кузнецов <and.kyz.mir@gmail.com>
2022-02-20 17:55:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication51/ConsoleApplication51.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+//вывод строчки пирамиды до n
+void piramid(int& n) { 
+    for (int i = 1; i < n; i++)
+        cout << i << " ";
+    cout << n;
+    n++;
+}
+
+int main()
+{
+    int N;
+    int n = 1;
+    cin >> N;
+    if (N > 9 or N < 1)
+        cout << "N is wrong: " << N;
+    else
+        do {
+            piramid(n);
+            if (n != N+1)
+                cout << endl; //избегаем переноса после вывода последней строчки
+        } while (n!=N+1);
+}
